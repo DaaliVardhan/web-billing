96a997e3eaa3dd16aab5694d67b1371bf1da324e
Daali Vardhan <Daali.Vardhan@coxautoinc.com>
2026-09-26 18:35:15 +0530

    add order delivery type salescard

--- src/components/SalesCards.tsx
+++ src/components/SalesCards.tsx
@@ -7,20 +7,39 @@ import {
 } from "@/components/ui/card"
 import type { Order } from "@/types"
 import { fallbackToZero } from "@/utils"
+import { Eye, EyeOff } from "lucide-react"
+import { useState } from "react"
 
 interface SectionCardProps {
   orders: Order[]
 }
 
 export function SectionCards({ orders }: SectionCardProps) {
+  const [showTotalSales, setShowTotalSales] = useState<boolean>(false)
   let totalSales = 0
   let totalShawarmaSold = 0
   let totalBowlsSold = 0
   let totalDrinksSold = 0
+  let totalNumberOfDining = 0
+  let totalNumberOfTakeaway = 0
+  let totalNumberOfZomato = 0
   const shawarmas: Record<string, number> = {}
 
   for (const order of orders) {
     totalSales += fallbackToZero(order.totalPrice)
+
+    switch (order.type) {
+      case "Zomato":
+        totalNumberOfZomato += 1
+        break
+      case "Takeaway":
+        totalNumberOfTakeaway += 1
+        break
+      case "Dining":
+      default:
+        totalNumberOfDining += 1
+    }
+
     Object.values(order.items).forEach((item) => {
       const name = item.name
         .toLowerCase()
@@ -46,17 +65,27 @@ export function SectionCards({ orders }: SectionCardProps) {
     })
   }
 
+  const totalSalesString = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+  }).format(totalSales)
+
   return (
     <div>
       <div className="mx-4 mt-4 flex flex-col gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs md:grid md:grid-cols-4 lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
         <Card className="@container/card">
           <CardHeader>
-            <CardDescription>Total Sales</CardDescription>
+            <CardDescription className="flex items-center justify-start gap-2">
+              <span>Total Sales</span>{" "}
+              <span
+                className="cursor-pointer"
+                onClick={() => setShowTotalSales((prev) => !prev)}
+              >
+                {!showTotalSales ? <Eye /> : <EyeOff />}
+              </span>
+            </CardDescription>
             <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-              {new Intl.NumberFormat("en-IN", {
-                style: "currency",
-                currency: "INR",
-              }).format(totalSales)}
+              {showTotalSales ? totalSalesString : "₹*.00"}
             </CardTitle>
           </CardHeader>
         </Card>
@@ -87,8 +116,8 @@ export function SectionCards({ orders }: SectionCardProps) {
         </Card>
       </div>
       {Object.keys(shawarmas).length > 0 && (
-        <div className="mx-4 mt-4 flex flex-col gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs md:grid md:grid-cols-1 lg:px-6 @xl/main:grid-cols-1 @5xl/main:grid-cols-1 dark:*:data-[slot=card]:bg-card">
-          <Card className="@container/card">
+        <div className="mx-4 mt-4 flex flex-col gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs md:grid md:grid-cols-4 lg:px-6 @xl/main:grid-cols-4 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
+          <Card className="@container/card col-span-3">
             <CardHeader>
               <CardDescription>Shawarma's Sold</CardDescription>
             </CardHeader>
@@ -104,6 +133,31 @@ export function SectionCards({ orders }: SectionCardProps) {
                 ))}
               </div>
             </CardContent>
+          </Card>
+          <Card className="@container/card">
+            <CardHeader>
+              <CardDescription>Orders fulfilled</CardDescription>
+              <ul className="mt-1">
+                <li>
+                  <span className="text-xl font-semibold">
+                    {totalNumberOfDining}
+                  </span>
+                  {"  "} Dining
+                </li>
+                <li>
+                  <span className="text-xl font-semibold">
+                    {totalNumberOfTakeaway}
+                  </span>
+                  {"  "} Takeaway
+                </li>
+                <li>
+                  <span className="text-xl font-semibold">
+                    {totalNumberOfZomato}
+                  </span>
+                  {"  "} Zomato
+                </li>
+              </ul>
+            </CardHeader>
           </Card>
         </div>
       )}{" "}
